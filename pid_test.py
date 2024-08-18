60d1b4138739be0ed551453d8c9fcae7911a747d
Moo Cow <moocowgalaxy@gmail.com>
2024-08-18 13:56:01 -0700

    update pid values

--- pid_test.py
+++ pid_test.py
@@ -3,9 +3,9 @@ from cococar_lib import CocoCar
 import time
 
 car = CocoCar()
-speed_pid = PIDController(0.0036, 0, 0.01)
-angle_pid = PIDController(0.0007, 0, 0.0003)
-max_output = 0.4
+speed_pid = PIDController(0.002, 0, 0.02)
+angle_pid = PIDController(0.0006, 0, 0.0003)
+max_output = 0.7
 position_setpoint = 320
 distance_setpoint = 40
 
@@ -15,13 +15,19 @@ def x_midpoint(a, b):
 
 
 speed = 0
+angle = 0
 no_detection_count = 0
 while True:
     markers = car.camera.detect_aruco_markers()
 
     if len(markers) == 0:
-        if no_detection_count == 5:
+        if no_detection_count == 10:
             car.drive.set_speed(0, 0)
+            speed = 0
+            angle = 0
+        speed *= 0.9
+        angle *= 0.9
+        car.drive.drive(speed, angle, max_output)
         speed_pid.get_output(0)
         angle_pid.get_output(0)  # so that derivative is also updating
         no_detection_count += 1
@@ -34,10 +40,12 @@ while True:
 
         distance_error = markers[0]['distance'] - distance_setpoint
 
-        speed += speed_pid.get_output(distance_error)
+        delta_speed = speed_pid.get_output(distance_error)
+        print(delta_speed)
+        speed += delta_speed
         speed = clamp(speed, -max_output, max_output)
-        angle_output = clamp(angle_pid.get_output(position_error), -max_output, max_output)
-        print(speed, angle_output, markers[0]['distance'])
-        car.drive.drive(speed, angle_output, max_output)
+        angle = clamp(angle_pid.get_output(position_error), -max_output, max_output)
+        print(speed, angle, markers[0]['distance'])
+        car.drive.drive(speed, angle, max_output)
 
-    time.sleep(0.1)
+    time.sleep(0.08)
