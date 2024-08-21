19a2fcaa537bb181509aa0e751b8725dbf791ddf
MooCowGalaxy <moo@c4n.net>
2024-08-20 22:35:59 -0700

    Update test drive code

--- test-drive.py
+++ test-drive.py
@@ -1,19 +1,17 @@
 from cococar_lib import CocoCar
-import time
 
 car = CocoCar()
 
 TURN_FACTOR = 0.6
 
-while True:
+
+def update():
     x = car.controller.get_channel(car.controller.RIGHT_X) * TURN_FACTOR
     y = car.controller.get_channel(car.controller.RIGHT_Y)
 
-    left = x + y
-    right = x - y
+    print(f'x: {x:.3f}, y: {y:.3f}')
 
-    print(f'x: {x:.3f}, y: {y:.3f}, left: {left:.3f}, right: {right:.3f}')
+    car.set_drive(y, x)
 
-    car.set_drive(x, y)
 
-    time.sleep(0.05)
+car.set_update_callback(update)
